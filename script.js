e162cf46e194570c67d325a4d991376e0c1c06ba
Sigolletes <juditreverter87@gmail.com>
2022-09-16 13:05:46 +0200

    Fix logical issues

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 // VARIABLES
 
+// DOM 
+
 const currentOperation = document.querySelector("#currentOperation");
 const result = document.querySelector("#result");
+
+// BUTTONS
 
 const clear = document.querySelector("#clear");
 const dlt = document.querySelector("#delete");
@@ -22,6 +26,8 @@ const zero = document.querySelector("#zero");
 const dot = document.querySelector("#dot");
 const equal = document.querySelector("#equal");
 
+// OPERATIONS
+
 let num1 = "";
 let num2 = "";
 let operand = "";
@@ -32,6 +38,8 @@ const regDot = /\./;
 
 
 // EVENTS
+
+// DELETE AND CLEAR
 
 clear.addEventListener("click", () => {
     num1 = "";
@@ -44,15 +52,15 @@ clear.addEventListener("click", () => {
 
 dlt.addEventListener("click", () => {
     if (num1.length === 1 && num2 === "") {
-        num1 = 0;
+        num1 = "0";
         register = register.slice(0, -1);
         currentOperation.textContent = register;
-        result.textContent = 0;
+        result.textContent = "0";
     } else if (num2.length === 1 && num1 !== "") {
-        num2 = 0;
+        num2 = "0";
         register = register.slice(0, -1);
         currentOperation.textContent = register;
-        result.textContent = 0;
+        result.textContent = "0";
     }  else if (num1 !== "" && num2 === "") {
         num1 = num1.slice(0, -1);
         register = register.slice(0, -1);
@@ -66,64 +74,24 @@ dlt.addEventListener("click", () => {
     }
 });
 
+
+
+// OPERANDS
+
 division.addEventListener("click", () => {
-    if (operand !== "" && num2 !== "" && num1 !== "") {
-        calculator();
-        operand = "/";
-        register += " / "; 
-        currentOperation.textContent = register;
-        result.textContent = num1;
-    } else if (num2 === "" && num1 !== "") {
-        operand = "/";
-        register += " / ";
-        currentOperation.textContent = register;
-        result.textContent = num1;
-    }
+    operandButton("/");
 });
 
 multiply.addEventListener("click", () => {
-    if (operand !== "" && num2 !== "" && num1 !== "") {
-        calculator();
-        operand = "*";
-        register += " * ";
-        currentOperation.textContent = register;
-        result.textContent = num1;
-    } else if (num2 === "" && num1 !== "") {
-        operand = "*";
-        register += " * ";
-        currentOperation.textContent = register;
-        result.textContent = num1;
-    }
+    operandButton("*");
 });
 
 subtract.addEventListener("click", () => {
-    if (operand !== "" && num2 !== "" && num1 !== "") {
-        calculator();
-        operand = "-";
-        register += " - ";
-        currentOperation.textContent = register;
-        result.textContent = num1;
-    } else if (num2 === "" && num1 !== "") {
-        operand = "-";
-        register += " - ";
-        currentOperation.textContent = register;
-        result.textContent = num1;
-    }
+    operandButton("-");
 });
 
 plus.addEventListener("click", () => {
-    if (operand !== "" && num2 !== "" && num1 !== "") {
-        calculator();
-        operand = "+";
-        register += " + ";
-        currentOperation.textContent = register;
-        result.textContent = num1;
-    } else if (num2 === "" && num1 !== "") {
-        operand = "+";
-        register += " + ";
-        currentOperation.textContent = register;
-        result.textContent = num1;
-    }
+    operandButton("+");
 });
 
 equal.addEventListener("click", () => {
@@ -131,6 +99,10 @@ equal.addEventListener("click", () => {
         calculator();
     }
 });
+
+
+
+// DOT FOR DECIMALS
 
 dot.addEventListener("click", () => {
     if (operand === "" && num2 === "" && num1 !== "" && num1.length <= 5 && !regDot.test(num1)) {
@@ -165,6 +137,10 @@ dot.addEventListener("click", () => {
 
 });
 
+
+
+// NUMBERS
+
 zero.addEventListener("click", () => {
 
     if (operand === "" && num2 === "" && num1 !== "" && num1.length <= 6) {
@@ -181,220 +157,95 @@ zero.addEventListener("click", () => {
 });
 
 one.addEventListener("click", () => {
-    if (num1 === "" && operand === "" && num2 === "") {
-        num1 = "1";
-        register = "1";
-        currentOperation.textContent = register;
-        result.textContent = "1";
-    } else if (operand === "" && num2 === "" && num1.length <= 6) {
-        num1 += "1";
-        register += "1";
-        currentOperation.textContent = register;
-        result.textContent += "1";
-    } else if (num1 !== "" && operand !== "" && num2 === "") {
-        num2 = "1";
-        register += "1";
-        currentOperation.textContent = register;
-        result.textContent = "1";
-    } else if (num1 !== "" && operand !== "" && num2.length <= 6) {
-        num2 += "1";
-        register += "1";
-        currentOperation.textContent = register;
-        result.textContent += "1";
-    }
+    numberButton("1");
 });
 
 two.addEventListener("click", () => {
-    if (num1 === "" && operand === "" && num2 === "") {
-        num1 = "2";
-        register = "2";
-        currentOperation.textContent = register;
-        result.textContent = "2";
-    } else if (operand === "" && num2 === "" && num1.length <= 6) {
-        num1 += "2";
-        register += "2";
-        currentOperation.textContent = register;
-        result.textContent += "2";
-    } else if (num1 !== "" && operand !== "" && num2 === "") {
-        num2 = "2";
-        register += "2";
-        currentOperation.textContent = register;
-        result.textContent = "2";
-    }  else if (num1 !== "" && operand !== "" && num2.length <= 6) {
-        num2 += "2";
-        register += "2";
-        currentOperation.textContent = register;
-        result.textContent += "2";
-    }
+    numberButton("2");
 });
 
 three.addEventListener("click", () => {
-    if (num1 === "" && operand === "" && num2 === "") {
-        num1 = "3";
-        register = "3";
-        currentOperation.textContent = register;
-        result.textContent = "3";
-    } else if (operand === "" && num2 === "" && num1.length <= 6) {
-        num1 += "3";
-        register += "3";
-        currentOperation.textContent = register;
-        result.textContent += "3";
-    } else if (num1 !== "" && operand !== "" && num2 === "") {
-        num2 = "3";
-        register += "3";
-        currentOperation.textContent = register;
-        result.textContent = "3";
-    }  else if (num1 !== "" && operand !== "" && num2.length <= 6) {
-        num2 += "3";
-        register += "3";
-        currentOperation.textContent = register;
-        result.textContent += "3";
-    }
+    numberButton("3");
 });
 
 four.addEventListener("click", () => {
-    if (num1 === "" && operand === "" && num2 === "") {
-        num1 = "4";
-        register = "4";
-        currentOperation.textContent = register;
-        result.textContent = "4";
-    } else if (operand === "" && num2 === "" && num1.length <= 6) {
-        num1 += "4";
-        register += "4";
-        currentOperation.textContent = register;
-        result.textContent += "4";
-    } else if (num1 !== "" && operand !== "" && num2 === "") {
-        num2 = "4";
-        register += "4";
-        currentOperation.textContent = register;
-        result.textContent = "4";
-    }  else if (num1 !== "" && operand !== "" && num2.length <= 6) {
-        num2 += "4";
-        register += "4";
-        currentOperation.textContent = register;
-        result.textContent += "4";
-    }
+    numberButton("4");
 });
 
 five.addEventListener("click", () => {
-    if (num1 === "" && operand === "" && num2 === "") {
-        num1 = "5";
-        register = "5";
-        currentOperation.textContent = register;
-        result.textContent = "5";
-    } else if (operand === "" && num2 === "" && num1.length <= 6) {
-        num1 += "5";
-        register += "5";
-        currentOperation.textContent = register;
-        result.textContent += "5";
-    } else if (num1 !== "" && operand !== "" && num2 === "") {
-        num2 = "5";
-        register += "5";
-        currentOperation.textContent = register;
-        result.textContent = "5";
-    }  else if (num1 !== "" && operand !== "" && num2.length <= 6) {
-        num2 += "5";
-        register += "5";
-        currentOperation.textContent = register;
-        result.textContent += "5";
-    }
+    numberButton("5");
 });
 
 six.addEventListener("click", () => {
-    if (num1 === "" && operand === "" && num2 === "") {
-        num1 = "6";
-        register = "6";
-        currentOperation.textContent = register;
-        result.textContent = "6";
-    } else if (operand === "" && num2 === "" && num1.length <= 6) {
-        num1 += "6";
-        register += "6";
-        currentOperation.textContent = register;
-        result.textContent += "6";
-    } else if (num1 !== "" && operand !== "" && num2 === "") {
-        num2 = "6";
-        register += "6";
-        currentOperation.textContent = register;
-        result.textContent = "6";
-    }  else if (num1 !== "" && operand !== "" && num2.length <= 6) {
-        num2 += "6";
-        register += "6";
-        currentOperation.textContent = register;
-        result.textContent += "6";
-    }
+    numberButton("6");
 });
 
 seven.addEventListener("click", () => {
-    if (num1 === "" && operand === "" && num2 === "") {
-        num1 = "7";
-        register = "7";
-        currentOperation.textContent = register;
-        result.textContent = "7";
-    } else if (operand === "" && num2 === "" && num1.length <= 6) {
-        num1 += "7";
-        register += "7";
-        currentOperation.textContent = register;
-        result.textContent += "7";
-    } else if (num1 !== "" && operand !== "" && num2 === "") {
-        num2 = "7";
-        register += "7";
-        currentOperation.textContent = register;
-        result.textContent = "7";
-    }  else if (num1 !== "" && operand !== "" && num2.length <= 6) {
-        num2 += "7";
-        register += "7";
-        currentOperation.textContent = register;
-        result.textContent += "7";
-    }
+    numberButton("7");
 });
 
 eight.addEventListener("click", () => {
-    if (num1 === "" && operand === "" && num2 === "") {
-        num1 = "8";
-        register = "8";
-        currentOperation.textContent = register;
-        result.textContent = "8";
-    } else if (operand === "" && num2 === "" && num1.length <= 6) {
-        num1 += "8";
-        register += "8";
-        currentOperation.textContent = register;
-        result.textContent += "8";
-    } else if (num1 !== "" && operand !== "" && num2 === "") {
-        num2 = "8";
-        register += "8";
-        currentOperation.textContent = register;
-        result.textContent = "8";
-    }  else if (num1 !== "" && operand !== "" && num2.length <= 6) {
-        num2 += "8";
-        register += "8";
-        currentOperation.textContent = register;
-        result.textContent += "8";
-    }
+    numberButton("8");
 });
 
 nine.addEventListener("click", () => {
-    if (num1 === "" && operand === "" && num2 === "") {
-        num1 = "9";
-        register = "9";
-        currentOperation.textContent = register;
-        result.textContent = "9";
-    } else if (operand === "" && num2 === "" && num1.length <= 6) {
-        num1 += "9";
-        register += "9";
-        currentOperation.textContent = register;
-        result.textContent += "9";
-    } else if (num1 !== "" && operand !== "" && num2 === "") {
-        num2 = "9";
-        register += "9";
-        currentOperation.textContent = register;
-        result.textContent = "9";
-    }  else if (num1 !== "" && operand !== "" && num2.length <= 6) {
-        num2 += "9";
-        register += "9";
-        currentOperation.textContent = register;
-        result.textContent += "9";
-    }
+    numberButton("9");
 });
+
+
+
+// FUNCTION FOR NUMBER BUTTONS
+
+function numberButton(num) {
+    if (register.length >= 25) {    
+        register = register.substring(1);
+    }
+
+    if (num1 === "" && operand === "" && num2 === "" || num1 === "0") {
+        num1 = num;
+        register = num;
+        currentOperation.textContent = register;
+        result.textContent = num;
+    } else if (operand === "" && num2 === "" && num1.length <= 6) {
+        num1 += num;
+        register += num;
+        currentOperation.textContent = register;
+        result.textContent += num;
+    } else if (num1 !== "" && operand !== "" && num2 === "" || num2 === "0") {
+        num2 = num;
+        register += num;
+        currentOperation.textContent = register;
+        result.textContent = num;
+    }  else if (num1 !== "" && operand !== "" && num2.length <= 6) {
+        num2 += num;
+        register += num;
+        currentOperation.textContent = register;
+        result.textContent += num;
+    }
+}
+
+
+
+// FUNCTION FOR OPERAND BUTTONS
+
+function operandButton(oper) {
+    if (register.length >= 25) {    
+        register = register.substring(3);
+    }
+
+    if (operand !== "" && num2 !== "" && num1 !== "") {
+        calculator();
+        operand = oper;
+        register += ` ${oper} `;
+        currentOperation.textContent = register;
+        result.textContent = num1;
+    } else if (num2 === "" && num1 !== "") {
+        operand = oper;
+        register += ` ${oper} `;
+        currentOperation.textContent = register;
+        result.textContent = num1;
+    }
+}
 
 
 
@@ -409,7 +260,7 @@ function calculator() {
         num2 = "";
         operand = "";
         result.textContent = num1;
-    } else if (operand == "-") {
+    } else if (operand === "-") {
         num1 = Number(num1) - Number(num2);
         if (regDot.test(num1)) {
             num1 = num1.toFixed(2);
@@ -417,7 +268,7 @@ function calculator() {
         num2 = "";
         operand = "";
         result.textContent = num1;
-    } else if (operand == "*") {
+    } else if (operand === "*") {
         num1 = Number(num1) * Number(num2);
         if (regDot.test(num1)) {
             num1 = num1.toFixed(2);
@@ -425,7 +276,7 @@ function calculator() {
         num2 = "";
         operand = "";
         result.textContent = num1;
-    } else if (operand == "/") {
+    } else if (operand === "/") {
         num1 = Number(num1) / Number(num2);
         if (regDot.test(num1)) {
             num1 = num1.toFixed(2);
@@ -434,4 +285,16 @@ function calculator() {
         operand = "";
         result.textContent = num1;
     }
+
+    if (num1.length >= 10) {
+        result.textContent = exponential(num1);
+    }
+}
+
+
+
+// FUNCTION FOR MAKE EXPONENTTIAL NUMBERS
+
+function exponential(num) {
+    return parseFloat(num).toExponential(4);
 }
